69594c4a1acc48050317557fe6e0f839c75951fe
rkannan2000 <rkannan2000@gmail.com>
2017-11-27 14:50:26 +1300

    Create m1_circumference.py

---
+++ m1_circumference.py
@@ -0,0 +1,9 @@
+# Calculate Circumference of a Circle with known radius
+# c = 2 * Pi * radius
+
+import math
+
+print('Enter Radius')
+r=input()
+c=2*math.pi*r
+print ('Circumference of the Circle with radius %d cms = %.2f'% (r,c))

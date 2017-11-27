1bf3010a42cc2fbe6bce758902fb9a0d6fc8d3de
rkannan2000 <rkannan2000@gmail.com>
2017-11-27 18:29:22 +1300

    Create m1_validate_names.py
    Includes # Extension exercises

---
+++ m1_validate_names.py
@@ -0,0 +1,90 @@
+# Your client has sent you a long list of names for use in your performance test scripts, the test engineer started writing a script
+# to validate the format of the names but had to leave and has asked you to finish it.
+# Your task is to complete the validate_name function to complete the script.
+
+
+
+def validate_name(name):
+    '''
+    Returns True if the name is valid, False if not based off the following rules
+    - the name has a max character length of 20
+    - the name consists of only alpha characters (ie a-zA-Z) and space characters
+    '''
+    result = 1
+
+    if (len(name) != 20):
+        result = 0
+    else:
+        for ch in name:
+            n=ord(ch)
+            if ( (n >= ord('a') and n <=ord('z')) or (n >= ord('A') and n <=ord('Z')) or n==10):
+                result = 1
+                continue
+            else:
+                result=0
+                #print('%s ****** n=%d   and Len=%d'% (name,n,len(name)))
+                break
+    return result
+
+# Extension exercises
+# - Complete the validate_name2 function
+# - Save the list of valid names to a file
+
+def validate_name2(name):
+    '''
+     Appends the results to the name file based on the Rule
+         if the name is valid, False if not based off the following rules
+    - the name has a max character length of 20
+    - the name consists of only alpha characters (ie a-zA-Z) and space characters
+    '''
+    result = 1
+
+    if (len(name) != 20):
+        result = 0
+    else:
+        for ch in name:
+            n=ord(ch)
+            if ( (n >= ord('a') and n <=ord('z')) or (n >= ord('A') and n <=ord('Z')) or n==10):
+                result = 1
+                continue
+            else:
+                result=0
+                #print('%s ****** n=%d   and Len=%d'% (name,n,len(name)))
+                break
+    if (result):
+        vf= open('valid_names.txt','a')
+        vf.write(name.rstrip())
+        vf.close()
+    else:
+        ef= open('invalid_names.txt','a')
+        ef.write(name.rstrip())
+        ef.close()
+    return result
+def main():
+    with open('names.txt') as f:
+        lines = f.readlines()
+    f.closed
+
+    valid_names = []
+    invalid_names = []
+
+    for line in lines:
+        name = line
+        valid = validate_name2(name)
+        print (valid)
+        if valid==True:
+            valid_names.append(name)
+        else:
+            invalid_names.append(name)
+
+    print("Total count of records imported: " + str((len(valid_names) + len(invalid_names))))
+    print("Count of imported records that meet the selected criteria: " + str(len(valid_names)))
+    print("list of valid names")
+    print("---------------------")
+
+
+    for name in valid_names:
+        print(name.rstrip())
+
+if __name__ == "__main__":
+    main()
